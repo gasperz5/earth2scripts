cb864e3553aff02b03fcf592309a8e9737aeba0d
Gašper Žitko <gasper.zitko@gmail.com>
2024-08-22 16:34:30 +0200

    Sort entries

--- C-raiding-report-notifications.js
+++ C-raiding-report-notifications.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         Raiding report from notificatios
-// @version      0.1.0
+// @version      0.1.1
 // @description  Checks failed and successful raids and puts them in a file
 // @author       GasperZ5 -- gasperz (Discord) -- gasper (7.5% code for E2)
 // @support      https://www.buymeacoffee.com/gasper
 // ==/UserScript==
 
 let react = getReactInstance();
+
+const PLAIN = false; // change to true if your spreadsheet doesn't format correctly
 
 let properties = {};
 let notifications = react.notificationHubStore.notificationStore.notifications;
@@ -34,12 +36,16 @@ for (let index = 0; index < notifications.length; index++) {
 }
 
 let csv = 'Home property,Home link/Victim name,Victim profile/Event type,Ether Amount,Cydroids,Datetime,Property,Property Link\n'
-Object.entries(properties).forEach(([homeId, victims]) => {
+let unsorted = Object.entries(properties);
+
+let sorted = unsorted.sort((b, a) => Object.values(a[1]).reduce((e, f) => e + f.reduce((c, d) => c + d.data?.etherAmount || 0, 0), 0) - Object.values(b[1]).reduce((e, f) => e + f.reduce((c, d) => c + d.data?.etherAmount || 0, 0), 0))
+
+sorted.forEach(([homeId, victims]) => {
 
     let victimArray = Object.entries(victims);
 
     let homeDescription = victimArray[0][1][0].data.homeLandfield.description.split(',').join('').slice(0, 30);
-    let homeLink = makePropertyLink(homeId);
+    let homeLink = makeItHyperlink(makePropertyLink(homeId),'Property');
     let totalHome = victimArray.reduce((a, b) => a + b[1].reduce((c, d) => c + d.data?.etherAmount || 0, 0), 0).toFixed(2);
 
     csv += `\n${homeDescription},${homeLink},,${totalHome}\n`
@@ -47,7 +53,7 @@ Object.entries(properties).forEach(([homeId, victims]) => {
     victimArray.forEach(([victimId, raids]) => {
 
         let victimName = raids[0].data.victim.username.split(',').join('').slice(0, 30);
-        let victimProfile = makeProfileLink(victimId);
+        let victimProfile = makeItHyperlink(makeProfileLink(victimId),'Profile');
         let victimSum = raids.reduce((c, d) => c + d.data?.etherAmount || 0, 0);
 
         csv += `,${victimName},${victimProfile},${victimSum}\n`
@@ -59,7 +65,7 @@ Object.entries(properties).forEach(([homeId, victims]) => {
             let cydroids = element.data.droidsCount;
             let datetime = element.created.slice(0, 16);
             let enemyDescription = element.data.enemyLandfield.description.split(',').join('').slice(0, 30);
-            let enemyLink = makeMapLink(element.data.enemyLandfield.id);
+            let enemyLink = makeItHyperlink(makeMapLink(element.data.enemyLandfield.id),'Map Link');
 
             csv += `,,${type},${ether},${cydroids},${datetime},${enemyDescription},${enemyLink}\n`
         }
@@ -86,11 +92,16 @@ function getReactInstance() {
 };
 
 function makePropertyLink(id) {
-    return '"=HYPERLINK("https://app.earth2.io/#propertyInfo/' + id + '"\;"Property")"';
+    return 'https://app.earth2.io/#propertyInfo/' + id;
 }
 function makeMapLink(id) {
-    return '"=HYPERLINK("https://app.earth2.io/#thegrid/' + id + '"\;"Map link")"';
+    return 'https://app.earth2.io/#thegrid/' + id;
 }
 function makeProfileLink(id) {
-    return '"=HYPERLINK("https://app.earth2.io/#profile/' + id + '"\;"Profile")"';
+    return 'https://app.earth2.io/#profile/' + id;
+}
+
+function makeItHyperlink(link, placeholder){
+    if(PLAIN) return link;
+    return '"=HYPERLINK("' + link + '"\;"'+placeholder+'")"'
 }
